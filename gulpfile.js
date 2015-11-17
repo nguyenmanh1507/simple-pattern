64b4230608a2538dea0f31197fbf78c0be4c15a6
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2015-11-17 16:28:39 +0700

    create gulp task

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var gulp        = require('gulp'),
+		sass        = require('gulp-sass'),
+		jshint      = require('gulp-jshint'),
+		jade        = require('gulp-jade'),
+		browserSync = require('browser-sync'),
+		sourcemaps  = require('gulp-sourcemaps')
+	;
+
+// Sass task
+gulp.task('sass', function() {
+	return gulp.src('sass/styles.scss')
+			.pipe(sourcemaps.init())
+			.pipe(sass().on('error', sass.logError))
+			.pipe(sourcemaps.write())
+			.pipe(gulp.dest('css'))
+			.pipe(browserSync.stream())
+		;
+});
+
+// JShint task
+gulp.task('lint', function() {
+	return gulp.src('js/*.js')
+			.pipe(jshint())
+			.pipe(jshint.reporter('jshint-stylish'))
+		;
+});
+
+// Reload browser after lint task complete
+gulp.task('js-watch', ['lint'], browserSync.reload);
+
+// Compile Jade
+gulp.task('templates', function() {
+	return gulp.src(['jade/index.jade'])
+			.pipe(jade({
+				pretty: true
+			}))
+			.pipe(gulp.dest('./'))
+		;
+});
+
+// BrowserSync
+gulp.task('serve', function() {
+
+	browserSync.init({
+		server: './'
+	});
+
+	gulp.watch('jade/**/*.jade', ['templates']);
+	gulp.watch('./*.html').on('change', browserSync.reload);
+	gulp.watch('sass/**/*.scss', ['sass']);
+	gulp.watch('js/**/*.js', ['js-watch']);
+
+});
+
+// Default task
+gulp.task('default', ['serve']);

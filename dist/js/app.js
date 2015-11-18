e508b8b910abe140437a32832c5ad330b4d48ffc
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2015-11-18 15:29:57 +0700

    update

---
+++ dist/js/app.js
@@ -0,0 +1,76 @@
+(function() {
+	'use strict';
+	/**
+	 * Main menu
+	 */
+	var mainMenu = $('.sf-menu');
+
+	// Create the reponsive menu by cloning
+	// the existing one.
+	mainMenu.clone().removeClass().addClass('rwd-menu').prependTo('.navbar-container nav');
+
+	// Initialize the superfish menu.
+	mainMenu.superfish();
+
+	// Toggle the responsive menu.
+	$('#rwd-trigger').on('click', function(e) {
+		e.preventDefault();
+		$('.rwd-menu').slideToggle();
+	});
+
+	/**
+	 * Main menu
+	 */
+	$('#animations').find('.demo-col').on('click', function() {
+		var $this = $(this);
+		$this.addClass('animated ' + $this.text());
+
+		window.setTimeout(function() {
+			$this.removeClass('animated ' + $this.text());
+		}, 1400);
+	});
+
+	/**
+	 * Tabs.
+	 */
+	$('.tabs-links a').on('click', function(e) {
+		e.preventDefault();
+
+		var $this = $(this),
+		// Get the target tab.
+				newTab = $(this).attr('href');
+
+		// Show the targeted tab content while hiding the rest.
+		$(newTab).show().siblings().hide();
+
+		// Add the active class to the parent list item while removing it from the rest.
+		$(this).parent('li')
+			.addClass('active')
+			.siblings()
+			.removeClass('active');
+	});
+
+	/**
+	 * Accordions.
+	 */
+	$('.accordion-element-title').on('click', function(e) {
+		e.preventDefault();
+
+		// Get the target accordion.
+		var targetAccordion = $(this).attr('href');
+
+		// Collapse all accordion elements.
+		$('.accordion-element-title').removeClass('active');
+		$('.accordion-element-content').hide();
+
+		// Show the targeted element only.
+		$(this).addClass('active');
+		$(targetAccordion).slideDown();
+	});
+
+	/**
+	 * Accordions.
+	 */
+	
+
+}());
